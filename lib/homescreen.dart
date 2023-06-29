6d07516324afd17baf59c065da1c34402770f713
Joshua <frozen.hell08@gmail.com>
2023-06-29 21:32:20 +0800

    sort [un]
    - sort button [not working]
    - list not sorting

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,3 +1,4 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:task_app/realm/taskmodel.dart';
 import 'package:logger/logger.dart';
@@ -6,6 +7,7 @@ import 'package:task_app/widgets/confirm_dialog.dart';
 import 'package:realm/realm.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:task_app/widgets/list_tile.dart';
+import 'dart:math' as math;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,6 +19,7 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final Logger logger = Logger();
   dynamic taskLists;
+  List<TaskItem> sortedTaskList = [];
   int completedTasksCount = 0;
   // -------------------- LOADING --------------------
   // bool _isLoading = true;
@@ -105,64 +108,94 @@ class _HomeScreenState extends State<HomeScreen> {
                 style: TextStyle(fontSize: 18),
               ),
             )
-          : ListView.builder(
-              itemCount: taskLists.length,
-
-              // -------------------- DISPLAY --------------------
-
-              itemBuilder: (context, index) {
-                final task = taskLists[index];
-                bool isChecked = task.isDone;
-
-                return ShadowedCard(
-                  title: task.title,
-                  description: task.description,
-                  cbState: isChecked,
-                  onEditPressed: () {
-                    logger.i('pressed edit');
-                    logger.i('index : $index');
-                    showBottomSheet(index);
-                  },
-                  onDeletePressed: () {
-                    logger.i('pressed delete');
-
-                    showDialog(
-                        context: context,
-                        builder: (context) {
-                          return ConfirmationDialog(
-                            title: 'Delete Item',
-                            message:
-                                'Are you sure you want to delete this item?',
-                            confirmText: 'Delete',
-                            cancelText: 'Cancel',
-                            onConfirm: () {
-                              logger.i('Deleted the item ${task.title}');
-                              setState(() {
-                                realm.write(() {
-                                  realm.delete<TaskItem>(task);
-                                });
-                                _fetchCount();
-                              });
-                              Navigator.pop(context);
-                            },
-                            onCancel: () {
-                              logger.i('Cancelled');
-                              Navigator.pop(context);
-                            },
-                          );
+          : Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      icon: Transform(
+                        alignment: Alignment.center,
+                        transform: Matrix4.rotationY(math.pi),
+                        child: const Icon(Icons.sort),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _sortByTitle().then((_) {
+                            logger.i('trying to sort');
+                            for(var t in sortedTaskList) {
+                              logger.i('inside pressed : ${t.title}');
+                            }
+                          });
                         });
-                  },
-                  onCBPressed: (value) => {
-                    setState(() {
-                      isChecked = value!;
-                      logger.i('checkbox clicked : $value');
-                      realm.write(() => task.isDone = isChecked);
-                      _fetchCount();
-                    }),
-                  },
-                );
-              },
-            ),
+                      }, 
+                    ),
+                  ],
+                ),
+              ),
+              // -------------------- DISPLAY --------------------
+              Expanded(
+                child: ListView.builder(
+                    itemCount: taskLists.length,
+              
+                    itemBuilder: (context, index) {
+                      final task = taskLists[index];
+                      bool isChecked = task.isDone;
+              
+                      return ShadowedCard(
+                        title: task.title,
+                        description: task.description,
+                        cbState: isChecked,
+                        onEditPressed: () {
+                          logger.i('pressed edit');
+                          logger.i('index : $index');
+                          showBottomSheet(index);
+                        },
+                        onDeletePressed: () {
+                          logger.i('pressed delete');
+              
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return ConfirmationDialog(
+                                  title: 'Delete Item',
+                                  message:
+                                      'Are you sure you want to delete this item?',
+                                  confirmText: 'Delete',
+                                  cancelText: 'Cancel',
+                                  onConfirm: () {
+                                    logger.i('Deleted the item ${task.title}');
+                                    setState(() {
+                                      realm.write(() {
+                                        realm.delete<TaskItem>(task);
+                                      });
+                                      _fetchCount();
+                                    });
+                                    Navigator.pop(context);
+                                  },
+                                  onCancel: () {
+                                    logger.i('Cancelled');
+                                    Navigator.pop(context);
+                                  },
+                                );
+                              });
+                        },
+                        onCBPressed: (value) => {
+                          setState(() {
+                            isChecked = value!;
+                            logger.i('checkbox clicked : $value');
+                            realm.write(() => task.isDone = isChecked);
+                            _fetchCount();
+                          }),
+                        },
+                      );
+                    },
+                  ),
+              ),
+            ],
+          ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => {
           logger.i("clicked"),
@@ -292,4 +325,18 @@ class _HomeScreenState extends State<HomeScreen> {
     });
   }
 
+  Future<List<TaskItem>> _sortByTitle() async {
+    final List<TaskItem> tempList = taskLists.toList();
+    tempList.sort((a, b) => a.title.compareTo(b.title));
+
+    // final rever = tempList.reversed;
+    final sortee = tempList.sortedBy((element) => element.description);
+
+    sortedTaskList = tempList;
+
+    for(var t in sortee) {
+      logger.i('rever : ${t.title}');
+    }
+    return sortedTaskList;
+  }
 }
